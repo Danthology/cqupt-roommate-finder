afd21b16a2e93bebd7922f53bb547aa5e2596b7d
Azurak <byron0511@outlook.com>
2018-07-30 10:50:08 +0800

    前端提交

--- public/room/index2.php
+++ public/room/index2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>重邮查室友</title>
-    <link rel="stylesheet" href="room/search.css">
+    <link rel="stylesheet" href="room/output.css">
     <meta name="description" content="给你未来的室友说句话吧~~">
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
