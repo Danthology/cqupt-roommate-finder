28fed7f6bf9f7c3c165b5d6729a794aee678ad2c
Azurak <byron0511@outlook.com>
2018-07-30 10:54:24 +0800

    前端提交

--- public/room/index2.php
+++ public/room/index2.php
@@ -103,7 +103,7 @@
         </div>
     </div>
 </div>
-<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.js"></script>
+<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
 <script>
     $.ajaxSetup({
         headers: {
